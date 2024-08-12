05e8bcc73d0755ff830cdd368007e6a8bfbf3f00
EloraBurton <EloraBurton@ELORASLAPTOP>
2024-08-11 21:17:47 -0400

    Final changes for now until I can fix the formatting better :D

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 let menu = document.querySelector('#mobile-menu');
 let menuLinks = document.querySelector('.navbar_menu');
+let menuItems = document.querySelectorAll('.navbar_links');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
+menuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        menu.classList.remove('is-active');
+        menuLinks.classList.remove('active');
+    });
+});
